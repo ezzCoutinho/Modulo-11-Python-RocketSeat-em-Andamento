9279c98458a830d04582a1a0f8a52486bb408d24
ezzcoutinho <lammerlopes@hotmail.com>
2025-05-06 21:47:34 -0300

    Adiciona a classe SQLiteConnectionHandler para gerenciar conexões com o banco de dados SQLite, incluindo métodos para conectar, fechar e obter a conexão.

---
+++ src/models/sqlite/settings/connection.py
@@ -0,0 +1,19 @@
+import sqlite3
+from sqlite3 import Connection as SQLiteConnection
+
+
+class SQLiteConnectionHandler:
+    def __init__(self) -> None:
+        self.__connection_string = "settings.db"
+        self.__conn = None
+
+    def connect(self) -> SQLiteConnection:
+        conn = sqlite3.connect(self.__connection_string, check_same_thread=False)
+        self.__conn = conn
+        return conn
+
+    def close(self) -> None:
+        self.__conn.close()
+
+    def get_connection(self) -> SQLiteConnection:
+        return self.__conn
