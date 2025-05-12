fc0add6164ac7acad8249353cc5a638af9be2a05
ezzcoutinho <lammerlopes@hotmail.com>
2025-05-12 20:36:23 -0300

    Adiciona métodos para busca de produtos na classe ProductFinder, integrando requisições HTTP e respostas.

--- src/data/product_finder.py
+++ src/data/product_finder.py
@@ -1,3 +1,5 @@
+from src.http_types.http_request import HttpRequest
+from src.http_types.http_response import HttpResponse
 from src.models.redis.interfaces.redis_repositories_interface import (
     RedisRepositoryInterface,
 )
@@ -14,3 +16,6 @@ class ProductFinder:
     ) -> None:
         self.__redis_repo = redis_repo
         self.__sqlite_repo = sqlite_repo
+
+    def find_by_name(self, http_request: HttpRequest) -> HttpResponse:
+        pass
