9515f06a338871db3f18be593c431f7eff06cda2
ezzcoutinho <lammerlopes@hotmail.com>
2025-05-12 21:04:04 -0300

    Implementa a lógica de busca de produtos na classe ProductFinder, incluindo métodos para encontrar produtos em cache e no banco de dados SQLite, além de formatar a resposta HTTP.

--- src/data/product_finder.py
+++ src/data/product_finder.py
@@ -18,4 +18,48 @@ class ProductFinder:
         self.__sqlite_repo = sqlite_repo
 
     def find_by_name(self, http_request: HttpRequest) -> HttpResponse:
-        pass
+        product_name = http_request.params.get("product_name")
+        product = None
+        product = self.__find_in_cache(product_name)
+
+        if not product:
+            product = self.__find_in_sql(product_name)
+            self.__insert_in_cache(product)
+
+        return self.__format_response(product)
+
+    def __find_in_cache(self, product_name: str) -> tuple:
+        product_info = self.__redis_repo.get_key(product_name)
+
+        if product_info:
+            product_info_list = product_info.split(",")
+            return (0, product_name, product_info_list[0], product_info_list[1])
+
+        return None
+
+    def __find_in_sql(self, product_name: str) -> tuple:
+        product_info = self.__sqlite_repo.find_product_by_name(product_name)
+
+        if not product_info:
+            raise Exception("Product not found")
+
+        return product_info
+
+    def __insert_in_cache(self, product: tuple) -> None:
+        product_name = product[1]
+        value = f"{product[2]},{product[3]}"
+        self.__redis_repo.insert_expire(product_name, value, 60)
+
+    def __format_response(self, product: tuple) -> HttpResponse:
+        return HttpResponse(
+            body={
+                "type": "Product",
+                "count": 1,
+                "attributes": {
+                    "name": product[1],
+                    "price": product[2],
+                    "quantity": product[3],
+                },
+            },
+            status_code=200,
+        )
