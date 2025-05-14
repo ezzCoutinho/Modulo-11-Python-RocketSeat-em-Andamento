366e1856367f70be3c0678a743c6585bdc0aa32a
ezzcoutinho <lammerlopes@hotmail.com>
2025-05-13 22:30:18 -0300

    Refatora a função product_finder_composer para utilizar funções específicas de recuperação de conexões com Redis e SQLite, aumentando a clareza e a modularidade do código.

--- src/main/composer/product_finder_composer.py
+++ src/main/composer/product_finder_composer.py
@@ -1,10 +1,13 @@
 from src.data.product_finder import ProductFinder
-from src.main.server.server_settings import redis_connection, sqlite_connection
+from src.main.server.connections import get_redis_connection, get_sqlite_connection
 from src.models.redis.repositories.redis_repository import RedisRepository
 from src.models.sqlite.repository.products_repository import ProductsRepository
 
 
 def product_finder_composer() -> ProductFinder:
+    redis_connection = get_redis_connection()
+    sqlite_connection = get_sqlite_connection()
+
     redis_conn = redis_connection.get_connection()
     sqlite_conn = sqlite_connection.get_connection()
 
